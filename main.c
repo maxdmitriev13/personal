a200b652200dfb6375f4a0553c6a414a6d1a4537
Maksim Dmitriev <159940181+maxdmitriev13@users.noreply.github.com>
2024-02-15 20:29:32 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,14 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	system("chcp 65001 > nul"); //utf-8
-	printf("Введите температуру в градусах Цельсия: ");
-	double temp_c;
-	scanf("%lf", &temp_c);
-	double temp_f = temp_c * 1.8 + 32;
-	printf("Температура в градусах Фарингейта: %.3lf", temp_f);	
+	system("chcp 65001 > nul");
+	//Просим ввести имя, если имя есть, то:
+	//Hello, Name!
+	//Если пустое имя, то:
+	//Hello, Stranger!
+	char name[31];
+	printf("Введите Ваше имя: ");
+	//scanf_s("%s", name, sizeof(name));
+	gets(name);
+	if (strlen(name) == 0)
+		printf("Hello, Stranger!");
+	else
+		printf("Hello, %s!\n", name);
+
 	return 0;
 }
