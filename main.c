c7462628705e40b1363be8fd9ae6d4e0830f33f4
Maksim Dmitriev <159940181+maxdmitriev13@users.noreply.github.com>
2024-02-16 19:10:59 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,41 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-		// введите любое число
-	// в ответ "на ветке столько-то ворон"
+	//программа находит корни квадратного уравнения
 	system("chcp 65001 > nul");
-	char* name;
-	int voron;
-	printf("Введите любое число ворон:\n");
-	scanf_s("%d", &voron, sizeof(voron));
-	switch (voron%100)	{
-		case 11:
-		case 12:
-		case 13:
-		case 14:
-			name = "ворон";
-			break;
-		default:
-			switch (voron%10)	{	
-		case 0:
-		case 5:
-		case 6:
-		case 7:
-		case 8:
-		case 9:	
-			name = "ворон";
-			break;
-		case 1:
-			name = "ворона";
-			break;
-		default: 
-			name = "вороны";
-			break;
-		}
-		}
-		printf("На ветке %d %s", voron, name);
+	int a;
+	int b;
+	int c;
+	int k1;
+	int k2;
+	printf("Введите числа a, b, c:\n");
+	scanf_s("%d", &a, sizeof(a));
+	scanf_s("%d", &b, sizeof(b));
+	scanf_s("%d", &c, sizeof(c));
+	printf("Квадратное уравнение: y = %dx^2 + x%d + %d\n", a, b, c);
+	int D = b*b - (4*a*c);
+	printf("Дискриминант = %d\n", D);
+	if (D < 0 )
+		printf("Квадратное уравнение не имеет решения\n");
+	if (D == 0) {
+		k1 = (-b) / (2*a);
+		printf("Квадратное уравнение имеет один корень = %d\n", k1);
+	}
+	if (D > 0) {
+		k1 = ((-b) + sqrt(D)) / (2*a);
+		k2 = ((-b) - sqrt(D)) / (2*a);
+		printf("Квадратное уравнение имеет два корня = %d и %d\n", k1, k2);
+	}
 	return 0;
 }
