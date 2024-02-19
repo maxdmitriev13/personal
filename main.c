34d76d58cfb5df3fe97bd3c54338b5d3e447896a
Maksim Dmitriev <159940181+maxdmitriev13@users.noreply.github.com>
2024-02-19 21:24:56 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+#include "file.h"
+//#define DL_OK(r) (2*PI*(r))
+//#define PL_OK(r) (PI*((r)*(r)))
 
 int main(int argc, char *argv[]) {
-	system("chcp 65001 > nul");
-	//найти НОД двух чисел
-	int a, b;
-	printf("Введите числа a, b: \n");
-	scanf("%d %d", &a, &b);
-	int x = 1;
-	while (x != 0)	{
-		x = a % b;
-		if (x == 0) {
-		printf("b = %d\n", b);
-		return 0;
-		}
-		a = b;
-		b = x;
-		
-	}
 	
+	
+	/* 
+1.Определить константу PI через макрос
+2.Определить макрофункцию для нахождения длины окружности по радиусу = 2*pi*r
+3.Определить макрофунцию для нахождения площади круга по радиусу pi*r^2
+4.Перенести созданные макросы в отдельный заголовочный файл, оформив его правильно.
+
+*/
+	system("chcp 65001 > nul");
+	double r = 10;
+	double dl = DL_OK(r);
+	double pl = PL_OK(r);
+	printf("Длина окружности = %.2f. Площадь круга = %.2f.", dl, pl);
+
+	return 0;
 }
