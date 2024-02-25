df279db6dfa0cdd2532b96496b83f573072f7a1f
Maksim Dmitriev <159940181+maxdmitriev13@users.noreply.github.com>
2024-02-25 13:50:34 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,24 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "file.h"
-//#define DL_OK(r) (2*PI*(r))
-//#define PL_OK(r) (PI*((r)*(r)))
 
 int main(int argc, char *argv[]) {
-	
-	
-	/* 
-1.Определить константу PI через макрос
-2.Определить макрофункцию для нахождения длины окружности по радиусу = 2*pi*r
-3.Определить макрофунцию для нахождения площади круга по радиусу pi*r^2
-4.Перенести созданные макросы в отдельный заголовочный файл, оформив его правильно.
-
-*/
-	system("chcp 65001 > nul");
-	double r = 10;
-	double dl = DL_OK(r);
-	double pl = PL_OK(r);
-	printf("Длина окружности = %.2f. Площадь круга = %.2f.", dl, pl);
-
+	#define NSIZE 8
+	int n[] = {10, 15, 7, 3, 21, 18, 30, 14};
+	//find max
+	int max = n[0];
+	for (int i=1; i < NSIZE; i++)
+		if (n[i] > max)
+			max = n[i];
+	printf("max: %d\n",max);
 	return 0;
 }
