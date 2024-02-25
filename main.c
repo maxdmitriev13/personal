876358b028b5d5d83eaf75fae8f6f39728beb7bc
Maksim Dmitriev <159940181+maxdmitriev13@users.noreply.github.com>
2024-02-25 16:23:04 +0300

    Update main.c

--- main.c
+++ main.c
@@ -5,33 +5,26 @@ int compare_ints(const void* a, const void* b)
 {
     int arg1 = *(const int*)a;
     int arg2 = *(const int*)b;
- 
-    if (arg1 > arg2) return -1;
-    if (arg1 < arg2) return 1;
+    if (arg1 < arg2) return -1;
+    if (arg1 > arg2) return 1;
     return 0;
- 
-    // return (arg1 > arg2) - (arg1 < arg2); // possible shortcut
- 
-    // return arg1 - arg2; // erroneous shortcut: undefined behavior in case of
-                           // integer overflow, such as with INT_MIN here
+
 }
 
 int main(int argc, char *argv[]) {
-	//find median value
-		#define NSIZE 12
-	int n[] = {10, 15, 7, 3, 21, 1000, 18, 30, 14, 17, 20, 33};
+	#define NSIZE 10
+	int n[] = {18, 15, 17, 3, 21, 1000, 18, 30, 15, 17};
 	qsort(n, NSIZE, sizeof(int), compare_ints);
+		//print array
 	for (int i=0; i < NSIZE; i++)
-		printf("%-5d", n[i]);
+		printf("%-4d", n[i]);
 	puts("");
-	
-	double median_value;
-	if (NSIZE%2 == 1) {
-		median_value = n[NSIZE/2];
+	int count_v;
+	for (int i=0; i < NSIZE-1; i++)
+		if (n[i] == n[i+1]) {
+		count_v++;
 		}
-	else {
-		median_value = (n[NSIZE/2] + n[NSIZE/2 -1] ) /2.0;
-		}
-	printf("%.2f", median_value);
+	int uniq = NSIZE - count_v;
+	printf("Uniq elements : %d", uniq);
 	return 0;
 }
