e4c6879933512be3e9932d627132788b37858c7b
Maksim Dmitriev <159940181+maxdmitriev13@users.noreply.github.com>
2024-02-25 14:50:05 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,24 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "file.h"
-//#define DL_OK(r) (2*PI*(r))
-//#define PL_OK(r) (PI*((r)*(r)))
+
+int compare_ints(const void* a, const void* b)
+{
+    int arg1 = *(const int*)a;
+    int arg2 = *(const int*)b;
+ 
+    if (arg1 > arg2) return -1;
+    if (arg1 < arg2) return 1;
+    return 0;
+ 
+    // return (arg1 > arg2) - (arg1 < arg2); // possible shortcut
+ 
+    // return arg1 - arg2; // erroneous shortcut: undefined behavior in case of
+                           // integer overflow, such as with INT_MIN here
+}
 
 int main(int argc, char *argv[]) {
+	//find median value
+		#define NSIZE 12
+	int n[] = {10, 15, 7, 3, 21, 1000, 18, 30, 14, 17, 20, 33};
+	qsort(n, NSIZE, sizeof(int), compare_ints);
+	for (int i=0; i < NSIZE; i++)
+		printf("%-5d", n[i]);
+	puts("");
+	
+	double median_value;
+	if (NSIZE%2 == 1) {
+		int i = (((double)NSIZE/2)-0.5);
+		median_value = n[i];
+		}
+	else {
+		median_value = (n[NSIZE / 2] + n[NSIZE / 2 -1] ) /2.0;
+		}
+	printf("%.2f", median_value);
+		
 	
 	
-	/* 
-1.Определить константу PI через макрос
-2.Определить макрофункцию для нахождения длины окружности по радиусу = 2*pi*r
-3.Определить макрофунцию для нахождения площади круга по радиусу pi*r^2
-4.Перенести созданные макросы в отдельный заголовочный файл, оформив его правильно.
-
-*/
-	system("chcp 65001 > nul");
-	double r = 10;
-	double dl = DL_OK(r);
-	double pl = PL_OK(r);
-	printf("Длина окружности = %.2f. Площадь круга = %.2f.", dl, pl);
 
 	return 0;
 }
