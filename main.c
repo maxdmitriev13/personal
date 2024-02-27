8f711646b5f6352107c425ff8820944be802d28e
Maksim Dmitriev <159940181+maxdmitriev13@users.noreply.github.com>
2024-02-27 18:44:42 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,14 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdarg.h>
+#include <math.h>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+int compare_ints(const void* a, const void* b)
+{
+    int arg1 = *(const int*)a;
+    int arg2 = *(const int*)b;
+ 
+    if (arg1 < arg2) return -1;
+    if (arg1 > arg2) return 1;
+    return 0;
+ 
+}
+
+double mediana(int n, ...){
+	int m[n];
+	va_list params;
+	va_start(params, n);
+	for(int i=0; i < n; i++) 
+		m[i] = va_arg(params, int);
+	va_end(params);
+	
+	double med;
+	qsort(m, n, sizeof(int),compare_ints);
+	if (n % 2 == 1)
+		med = m[n/2];
+	else
+		med = ((m[(n/2) -1] + m[n /2] )/ 2.0);
+	return med;
+}
 
 int main(int argc, char *argv[]) {
-	system("chcp 65001 > nul"); //utf-8
-	printf("Введите температуру в градусах Цельсия: ");
-	double temp_c;
-	scanf("%lf", &temp_c);
-	double temp_f = temp_c * 1.8 + 32;
-	printf("Температура в градусах Фарингейта: %.3lf", temp_f);	
+	printf("med(10,11,21,13,24, 25,26,27) = %.2f\n", mediana(8,10,11,21,13,24, 25,26,27));
 	return 0;
 }
